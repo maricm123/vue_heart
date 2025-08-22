cbb9d302bfcf6090897dc4bbddaaf3279b5e2a6f
mmaric123 <mihailomaric001@gmail.com>
2025-08-22 15:04:19 +0200

    added capacitor for android

---
+++ android/app/src/main/java/com/heart/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.heart.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.heart.app',
+  appName: 'heartapp',
+  webDir: 'dist'
+};
+
+export default config;
