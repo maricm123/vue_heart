069b6de21ffdabc8e7590654183163130371af89
mmaric123 <mihailomaric001@gmail.com>
2026-01-06 13:45:13 +0100

    tidied dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,44 +7,38 @@ import StatsWidget from '@/components/dashboard/StatsWidget.vue';
 import { getDashboardInfo } from '@/services/dashboardService';
 import { ref, computed, onMounted } from 'vue';
 
-const dashboardInfo = ref(null)
-const loading = ref(true)
+const dashboardInfo = ref(null);
+const loading = ref(true);
 
 const displayName = computed(() => {
-    if (loading.value) return '...'
-    if (!dashboardInfo.value?.coach_name) return 'Guest'
-    return dashboardInfo.value.coach_name
-})
+    if (loading.value) return '...';
+    if (!dashboardInfo.value?.coach_name) return 'Guest';
+    return dashboardInfo.value.coach_name;
+});
 
 onMounted(async () => {
     try {
-        dashboardInfo.value = await getDashboardInfo()
+        dashboardInfo.value = await getDashboardInfo();
     } catch (e) {
-        dashboardInfo.value = null
+        dashboardInfo.value = null;
     } finally {
-        loading.value = false
+        loading.value = false;
     }
 });
-
 </script>
 
 <template>
     <div class="grid grid-cols-12 gap-8">
         <div class="col-span-12 mb-6">
-            <div class="p-6 rounded-xl shadow 
-            bg-white dark:bg-surface-900 
-            flex justify-between items-center">
-    <div>
-        <h1 class="text-3xl font-bold 
-                   text-surface-900 dark:text-surface-0">
-            Welcome, {{ displayName }}!
-        </h1>
+            <div class="p-6 rounded-xl shadow bg-white dark:bg-surface-900 flex justify-between items-center">
+                <div>
+                    <h1 class="text-3xl font-bold text-surface-900 dark:text-surface-0">Welcome, {{ displayName }}!</h1>
 
-        <p class="text-surface-600 dark:text-surface-400 text-lg">
-            Here’s what’s happening in <span class="font-bold">{{ dashboardInfo?.gym_name }}</span> right now.
-        </p>
-    </div>
-</div>
+                    <p class="text-surface-600 dark:text-surface-400 text-lg">
+                        Here’s what’s happening in <span class="font-bold">{{ dashboardInfo?.gym_name }}</span> right now.
+                    </p>
+                </div>
+            </div>
         </div>
 
         <StatsWidget />
